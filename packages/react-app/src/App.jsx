b45b4636ba193deb6e0c35775a120ad5c8808b95
yehorrudytsia <yehorrudytsia@gmail.com>
2022-01-25 12:12:18 +0200

    Change menu component

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -269,12 +269,7 @@ function App(props) {
         <Menu.Item key="/exampleui">
           <Link to="/exampleui">ExampleUI</Link>
         </Menu.Item>
-        <Menu.Item key="/mainnetdai">
-          <Link to="/mainnetdai">Mainnet DAI</Link>
-        </Menu.Item>
-        <Menu.Item key="/subgraph">
-          <Link to="/subgraph">Subgraph</Link>
-        </Menu.Item>
+
       </Menu>
 
       <Switch>
